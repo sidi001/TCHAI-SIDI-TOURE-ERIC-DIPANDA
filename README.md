8a95f30abbef8e35d0f9f5715a641da1810703f6
TOURE Sidi <siditouredjenne@gmail.com>
2020-12-21 19:40:08 +0100

    fin exo 8 avec suppression transaction

--- README.md
+++ README.md
@@ -39,3 +39,26 @@ NB: le lien http://0.0.0.0:8981/ utilisé pour chaque script ou url de teste dep
 - curl -d '{"personne1": "steve", "personne2": "keita", "montant":"10"}' -H "Content-Type: application/json" -X POST   http://0.0.0.0:8981/attaque
 
 message retourner si ça marche ==> "sommes tansactions are attaqued."
+
+============= fini la version 1 du projet à ce niveau==============
+
+============== debut version 2 de Tchai ===========
+- choix fonction de Hachage:
+    SHA-2 est une famille de fonctions de hachage qui ont été conçues par la National Security Agency des États-Unis, sur le modèle des fonctions SHA-1 et SHA-0, elles-mêmes fortement inspirées de la fonction MD4 de Ron Rivest (qui a donné parallèlement MD5).
+    Dans le code, on utilisera le sha224 car il est le plus simple des autres SHA-2 avec la taille du haché est indiquée par le suffixe : 224 bits pour SHA-224
+
+## Exercice 5
+### Modifier votre programme afin d’intégrer la nouvelle structure des transactions (P1, P2, t, a).
+- on mettra à la place du "a", le hash SHA224 defini precedemment
+
+## Exercice 5
+### Vérifier l’intégrité des données en recalculant les hashs à partir des données et en les comparant avec les hashs stockés précédemment.
+- pour cela, on a mis en place un API accessible avec l'url : http://0.0.0.0:8981/integrite 
+- cette api retourne une phrse nous confirmant que notre database est bien correcte, ainsi on pourra conclure sur l'integrité en même temps.
+
+## EXERCICE 7. on verifie bien que l’attaque précédente ne fonctionne plus. car le test d'integrité retourne que les données sont incorrect après les modification
+
+## EXERCICE 8. Attaquer le système en modifiant directement le fichier de données, en supprimant unetransaction. 
+- curl -d '{"personne1": "Robert", "personne2": "Natalie", "montant":"29"}' -H "Content-Type: application/json" -X POST   http://0.0.0.0:898/supprimer
+
+============= fini la version 2 du projet à ce niveau==============
--- SystemeTchai/Code_Python/Tchai_with_Flask.py
+++ SystemeTchai/Code_Python/Tchai_with_Flask.py
@@ -2,6 +2,7 @@ from flask import *
 import csv
 import pandas as pd
 from datetime import datetime
+import hashlib
 
 # import os
 # path = os.getcwd()
@@ -45,7 +46,13 @@ def listesTransactions():
     ) + '</ul>\n', 200
 
 """
-curl -d '{"personne1": "name_sender", "personne2": "name_receiver", "montant":"20"}' -H "Content-Type: application/json" -X POST   http://0.0.0.0:898/save
+Robert|Natalie|22|12/21/2020, 15:24:48|f36e498739b755a704dfe52ca12f988250a43d2cfcdf1cc601a7dc87
+Bernard|Robert|37|12/21/2020, 15:26:03|77cfd7f5b0d4b068d8ea28391e04e59e365bc4623e57c31082613cd6
+Natalie|Bernard|29|12/21/2020, 15:26:30|2f230c031e1c44c059caa16f4c1e87fca060d3fc22672910dd304765
+linuxize|linuxize@example.com|20|12/21/2020, 15:27:05|54a05e679466f0efd5999457bbb939a43012b172d778b3cc8d34db3b
+linuxize|linuxize@example.com|20|12/21/2020, 15:27:10|00541f12b9bccbd3c15e2bf4b031603c85cc17e05a761aec4f3f3909
+steve|keita|20|12/21/2020, 15:27:39|9e5333d0e667242fee03f6a6e49b4bafc10f587f9e52775a1337b793
+curl -d '{"personne1": "Robert", "personne2": "Natalie", "montant":"22"}' -H "Content-Type: application/json" -X POST   http://0.0.0.0:8981/save
 """
 @app.route('/save', methods=['POST'])
 def add():
@@ -57,7 +64,8 @@ def add():
     send.append(data["personne2"])
     send.append(data["montant"])
     send.append(date_time)
-    print(date_time)
+    h = hashlib.sha224(",".join(send)).hexdigest()
+    send.append(h)
     ##a prendre en compte que la direction ci dessous particulier au pc 
     with open('./SystemeTchai/Data_Base/transactions.csv', 'a') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter='|', quoting=csv.QUOTE_MINIMAL)
@@ -82,9 +90,34 @@ def attaquer():
     df.loc[(df['personne1']==str(data["personne1"])) & (df['personne2']==str(data["personne2"])),"montant"] = data["montant"]
     
     print(df)
-    df.to_csv(file_name0, sep = "|")
+    df.to_csv(file_name0, sep = "|", index_col = False)
 
     return 'sommes tansactions are attaqued.\n', 201
+
+"""
+curl -d '{"personne1": "Robert", "personne2": "Natalie", "montant":"29"}' -H "Content-Type: application/json" -X POST   http://0.0.0.0:898/supprimer
+"""
+@app.route('/supprimer', methods=['POST'])
+def supprimer():
+    file_name0 = "./SystemeTchai/Data_Base/transactions.csv"
+    transactions = []
+    data = request.get_json()
+    with open('./SystemeTchai/Data_Base/transactions.csv') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter='|')
+        for row in spamreader:
+            if row[0]=='personne1' :
+                 continue
+            
+            if row[0]==str(data["personne1"]) and row[1]==str(data["personne2"]) and str(row[2])==str(data["montant"]) :
+                continue
+
+            transactions.append(row)
+        
+        df = pd.DataFrame(transactions, columns=["personne1","personne2","montant","date","hash"])
+
+    df.to_csv(file_name0, index = False, sep = "|", columns=None)
+
+    return 'sommes tansactions are removed.\n', 201
 
 
 
@@ -118,6 +151,24 @@ def mes_soldes(uname):
         ['<li> ' + str(solde)]
     ) + '</ul>\n', 200
 
+@app.route('/integrite', methods=['GET'])
+def integrite():
+    data = "correct"
+    with open('./SystemeTchai/Data_Base/transactions.csv') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter='|')
+        for row in spamreader:
+            if row[0]=='personne1' :
+                continue
 
-app.run(host='0.0.0.0',port='8981', debug=True)
+            
+            h = hashlib.sha224( ",".join(row[0:4])).hexdigest()
+            if h != row[4] :
+                data = "incorrect"
+
+
+    return 'les donnees sont actuellemnt ' + str(data), 200
+
+
+
+app.run(host='0.0.0.0',port='898', debug=True)
 print("alors quoi de neuf")
